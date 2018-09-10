4f0847d7dcb42827360eb86583cd7e26d1340519
Unknown <benjamin.loriot@etu.utc.fr>
2018-09-10 18:26:21 +0200

    Codebuff + Naturalize implementation

--- python/Experiment.py
+++ python/Experiment.py
@@ -4,12 +4,34 @@ import threading
 import uuid
 import os
 import time
+import shutil
+import subprocess
 
 import json
 
 from Corpus import *
 
 pp = pprint.PrettyPrinter(indent=4)
+
+_GROUP = None
+
+def set_experiment_group(name):
+    _GROUP = name
+
+def get_files_path(dir):
+    print(dir)
+    paths = []
+    for path, subdirs, files in os.walk(dir):
+        for name in files:
+            paths.append(os.path.join(path, name))
+    return paths
+
+def call_java(jar, args):
+    cmd = "java -jar {} {}".format(jar, " ".join(args))
+    # print(cmd)
+    process = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    return output
 
 # add multithreading + experiment_pool
 class Experiment(threading.Thread):
@@ -20,9 +42,10 @@ class Experiment(threading.Thread):
         self.name = name
         self.parameters = dict()
         self.id = uuid.uuid4().hex
-        self.base_dir = "./experiments/" + name + "_" + self.id + "/"
+        self.base_dir = "./experiments/" + name + "/" + corpus.name + "_" + self.id + "/"
         self.results = None
-        self.training_files = self.corpus.copy_training_files(self.get_dir( "training_files/"))
+        self.training_files_dir = self.get_dir( "training_files/" )
+        self.training_files = self.corpus.copy_training_files(self.training_files_dir)
         self.date = time.time()
 
     def get_informations(self):
@@ -56,6 +79,9 @@ class Experiment(threading.Thread):
         else :
             print("no results to present")
 
+    def clean_up(self):
+        shutil.rmtree(self.base_dir);
+
     def log(self, message):
         print("[" + self.name + "_" + self.id + "]" + message)
 
@@ -69,15 +95,22 @@ class Experiment(threading.Thread):
 class Exp_Uglify(Experiment):
 
     def __init__(self, corpus, modification_number, iterations = 1):
-        Experiment.__init__(self, corpus, "uglify-" + corpus.name)
+        Experiment.__init__(self, corpus, "uglify")
         self.add_parameter("iterations", iterations)
         self.add_parameter("modification_number", modification_number)
-        self.left_out_files = self.corpus.copy_left_out_files(self.get_dir( "left_out_files/"))
+        self.left_out_files_dir = self.get_dir( "left_out_files/")
+        self.left_out_files = self.corpus.copy_left_out_files(self.left_out_files_dir)
 
     def get_informations(self):
         informations = Experiment.get_informations(self)
         informations["left_out_files"] = self.left_out_files
         return informations
+
+    def call_naturalize(self, training_dir, files_dir, output_dir):
+        return call_java("../jars/naturalize.jar", [training_dir, output_dir, files_dir])
+
+    def call_codebuff(self, training_dir, files_dir, output_dir):
+        return call_java("../jars/codebuff-1.5.1.jar", ["-g org.antlr.codebuff.Java8", "-rule compilationUnit", "-corpus " + training_dir, "-files java", "-comment LINE_COMMENT", "-indent 2", "-o " + output_dir, files_dir])
 
     def run(self):
         self.log("starting")
@@ -92,13 +125,37 @@ class Exp_Uglify(Experiment):
             for f in test_files:
                 ugly_files.append(juglify.gen_ugly(f, self.get_dir( "ugly/" + str(i) + "/"), modification_number=self.get_parameter("modification_number")))
 
-        (res, errors) = checkstyle.check(self.corpus.checkstyle, " ".join(ugly_files))
+        # Naturalize
+        self.call_naturalize(self.training_files_dir, self.get_dir( "ugly/"), self.get_dir( "naturalize/") )
+        naturalized_files = get_files_path(self.get_dir( "naturalize/"))
 
+        # Codebuff
+        self.call_codebuff(self.training_files_dir, self.get_dir( "ugly/"), self.get_dir( "codebuff/") )
+        codebuffed_files = get_files_path(self.get_dir( "codebuff/"))
+
+        (res, errors) = checkstyle.check(self.corpus.checkstyle, " ".join(ugly_files))
         res = Exp_Uglify.rename_checkstyle_output(res, ugly_files)
 
         self.results = { "errors": errors, "details": {key:len(res[key]["errors"]) for key in ugly_files} }
         self.results["errors_per_file"] = errors / len(ugly_files)
         self.results["corrupted_file_proportion"] = sum([ len(res[file]["errors"]) > 0 for file in res]) / len(ugly_files)
+
+        self.results["naturalize"] = dict()
+        try:
+            (res, errors) = checkstyle.check(self.corpus.checkstyle, " ".join(naturalized_files))
+            res = Exp_Uglify.rename_checkstyle_output(res, naturalized_files)
+            self.results["naturalize"]["corrupted_file_proportion"] = sum([ len(res[file]["errors"]) > 0 for file in res]) / len(naturalized_files)
+        except:
+            self.log("Error, cannot parse the naturaliz output")
+
+        self.results["codebuff"] = dict()
+        try:
+            (res, errors) = checkstyle.check(self.corpus.checkstyle, " ".join(codebuffed_files))
+            res = Exp_Uglify.rename_checkstyle_output(res, codebuffed_files)
+
+            self.results["codebuff"]["corrupted_file_proportion"] = sum([ len(res[file]["errors"]) > 0 for file in res]) / len(codebuffed_files)
+        except:
+            self.log("Error, cannot parse the codebuff output")
 
 
         return self.results
--- python/run-tests.py
+++ python/run-tests.py
@@ -3,42 +3,76 @@ import os
 from Corpus import *
 from Experiment import *
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# from matplotlib import pyplot
+
 pp = pprint.PrettyPrinter(indent=4)
 
 def list_corpora(corpora_dir):
     folders = next(os.walk(corpora_dir))[1]
     corpora = list()
     for folder in folders:
-        corpora.append( CorpusLeaveKOut(os.path.join(corpora_dir, folder) , folder, 1) )
+        for i in range(20):
+            corpora.append( CorpusLeaveKOut(os.path.join(corpora_dir, folder) , folder, 1) )
     return corpora
 
 if __name__ == "__main__":
     corpora = list_corpora("./test_corpora")
     # if os.path.exists("./experiments"):
     #     shutil.rmtree("./experiments")
+
+    starts = time.time()
+
+    experiments = []
+
     for corpus in corpora:
         print(corpus)
-        experiments = []
 
-        starts = time.time()
+        experiments.append(Exp_Uglify(corpus, 1, iterations = 25))
 
-        for i in range(1):
-            experiments.append(Exp_Uglify(corpus, 1, iterations = 200))
+    for exp in experiments:
+        exp.save_informations()
+        exp.daemon = True
+        exp.start()
 
-        for exp in experiments:
-            exp.save_informations()
-            exp.daemon = True
-            exp.start()
+    res = []
+    res_naturalize = []
+    res_codebuff = []
 
-        res = []
 
-        for exp in experiments:
-            exp.join()
-            exp.save_results()
-            res.append(exp.results["corrupted_file_proportion"])
+    for exp in experiments:
+        exp.join()
+        exp.save_results()
+        res.append(exp.results["corrupted_file_proportion"])
+        if "corrupted_file_proportion" in exp.results["naturalize"]:
+            res_naturalize.append(exp.results["naturalize"]["corrupted_file_proportion"])
+        if "corrupted_file_proportion" in exp.results["codebuff"]:
+            res_codebuff.append(exp.results["codebuff"]["corrupted_file_proportion"])
 
-        ends = time.time()
+    # for exp in experiments:
+    #     exp.clean_up()
 
-        print(ends - starts)
+    ends = time.time()
 
-        print(res)
+    print(ends - starts, "s")
+
+    print(res, res_naturalize, res_codebuff)
+
+    # Create a figure instance
+    fig = plt.figure(1, figsize=(6, 4))
+
+    # Create an axes instance
+    ax = fig.add_subplot(111)
+
+
+    # Create the boxplot
+    bp = ax.boxplot([res, res_naturalize, res_codebuff])
+
+    ax.set_ylim(0,1)
+    xtickNames = plt.setp(ax, xticklabels=["jUglify", "Naturalize", "Codebuff"])
+    plt.setp(xtickNames, rotation=45, fontsize=8)
+
+    plt.show()
