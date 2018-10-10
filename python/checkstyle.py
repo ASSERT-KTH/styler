5e3c3da91879ff3c9c0a45538f22839a35cf3948
Benjamin Loriot <benjamin.loriot@etu.utc.fr>
2018-10-10 09:44:04 +0200

    BAsic audit options

--- python/checkstyle.py
+++ python/checkstyle.py
@@ -8,6 +8,7 @@
 import xml.etree.ElementTree as ET
 import subprocess
 import sys
+from functools import reduce
 
 _CHECKSTYLE_JAR = "../jars/checkstyle-8.12-all.jar"
 
@@ -34,12 +35,56 @@ def parse_res(output):
                 output_parsed[elem_file.attrib['name']]['errors'].append(elem_error.attrib)
     return output_parsed
 
-if __name__ == "__main__":
-    checkstyle_path = "./test_corpora/commons-lang/checkstyle.xml"
-    file_path = "./ugly/java-design-patterns/Target.java ./ugly/java-design-patterns/Client.java"
-    if ( len(sys.argv) >= 3):
-        checkstyle_path = sys.argv[1]
-        file_path = sys.argv[2]
+def parse_file(file_path):
+    with open(file_path) as f:
+        file_content = f.read()
+    return parse_res(file_content)
 
-    (output_raw, errorcode) = check(checkstyle_path, file_path)
-    print(output_raw, errorcode)
+def analyse_results(results):
+    FIXABLE = (
+        'RegexpSinglelineCheck',
+        'WitespaceAfterCheck',
+        'OneStatementPerLineCheck',
+        'NoLineWrapCheck',
+        'OperatorWrapCheck',
+        'ParentPadCheck',
+        'NoWhitespaceBeforeCheck',
+        'FileTabCharacterCheck',
+        'IndentationCheck',
+    )
+
+    # sanitize the results
+    is_java = lambda file: file.split('.')[-1] == 'java'
+    results_sanitized = {key:value for key, value in results.items() if is_java(key)}
+
+    # count some stuff
+    number_of_files = len(results_sanitized)
+    files_with_errors = { key:value for key, value in results_sanitized.items() if len(value["errors"]) }
+    error_to_name = lambda error: error['source'].split('.')[-1]
+    errors = map(error_to_name, reduce(list.__add__, map(lambda x: x["errors"],files_with_errors.values())));
+    errors_count = {}
+    for error in errors:
+        errors_count[error] = errors_count.get(error, 0) + 1
+    total_number_of_errors = sum(errors_count.values())
+    number_of_files_error_free = number_of_files - len(files_with_errors)
+    possibly_fixable_count = reduce(lambda x, y: x + errors_count.get(y, 0),FIXABLE, 0)
+    return {
+        "number_of_files": number_of_files,
+        "number_of_files_error_free": number_of_files_error_free,
+        "errors_count": errors_count,
+        "total_number_of_errors": total_number_of_errors,
+        "possibly_fixable_count": possibly_fixable_count,
+    }
+
+if __name__ == "__main__":
+    if sys.argv[1] == "cs":
+        checkstyle_path = "./test_corpora/commons-lang/checkstyle.xml"
+        file_path = "./ugly/java-design-patterns/Target.java ./ugly/java-design-patterns/Client.java"
+        if ( len(sys.argv) >= 4):
+            checkstyle_path = sys.argv[2]
+            file_path = sys.argv[3]
+
+        (output_raw, errorcode) = check(checkstyle_path, file_path)
+        print(output_raw, errorcode)
+    elif sys.argv[1] == "read":
+        print(analyse_results(parse_file(sys.argv[2])))
