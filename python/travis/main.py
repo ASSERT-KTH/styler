e392311646432f3c7be1ca5fc0568c1e27a872c3
Benjamin Loriot <benjamin.loriot@etu.utc.fr>
2018-11-05 15:46:11 +0100

    tar the repo data

--- python/travis/main.py
+++ python/travis/main.py
@@ -2,6 +2,9 @@ import requests
 import sys
 import json
 import os
+import os
+import shutil
+import tarfile
 
 __base_dir = "/home/benjaminl/Documents/travis-ci-build-log-dataset"
 
@@ -13,16 +16,18 @@ def get_travis(endpoint, payload={}):
     print(f'Get {r.url}')
     return r;
 
-def get_builds(repo):
+def get_builds(repo, max_builds_collected=100):
     result = []
     after_number = 0
-    while after_number != 1:
+    while after_number != 1 and len(result) < max_builds_collected:
         if after_number:
             payload = {'after_number': after_number}
         else:
             payload = {}
         result += get_travis(f'/repos/{repo}/builds', payload=payload).json()
         after_number = int(result[-1]["number"])
+    if len(result) > max_builds_collected:
+        result = result[:max_builds_collected]
     return result
 
 def get_build(build):
@@ -59,15 +64,32 @@ def download_build_info(dir, build_id):
     jobs_id = [ job['id'] for job in jobs ]
     for job_id in jobs_id:
         download_job_info(folder, job_id)
+    tar_and_delete(folder)
 
 def download_repo_info(dir, repo):
     folder = os.path.join(dir, repo)
     if not os.path.exists(folder):
         os.makedirs(folder)
-    builds = get_builds(repo)
+    builds = get_builds(repo, max_builds_collected=100)
     builds_id = [ build['id'] for build in builds ]
     for build_id in builds_id:
         download_build_info(folder, build_id)
+
+def tar_and_delete(path):
+    tar_dir(path)
+    delete_dir(path)
+
+def delete_dir(path):
+    shutil.rmtree(path)
+
+def tar_dir(path):
+    folder_name = path.split('/')[-1]
+    parent_dir = "/".join(path.split('/')[:-1])
+    with tarfile.open(f'{parent_dir}/{folder_name}.tar.gz', "w:bz2") as tar_handle:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                tar_handle.add(file_path, arcname=file_path[len(path):])
 
 if __name__ == "__main__":
     if not os.path.exists(__base_dir):
