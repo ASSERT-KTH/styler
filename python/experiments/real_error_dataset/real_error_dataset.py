5e57250e23ddb52ff357ae359b0cdfe4796978a0
Fer Madeiral <fer.madeiral@gmail.com>
2020-07-07 13:10:11 +0200

    Improve selection of real errors

--- python/experiments/real_error_dataset/real_error_dataset.py
+++ python/experiments/real_error_dataset/real_error_dataset.py
@@ -21,7 +21,7 @@ __dataset_dir = config['DEFAULT']['dataset_dir']
 def load_errors_info(only_targeted=False):
     def filepath_from_json_path(x):
         return safe_get_first(glob.glob(pathname=f'{x.rpartition("/")[0]}/*.java'))
-    error_json_path = glob.glob(pathname=f'{__checkstylerr_checkedout_projects_dir}/*/*/*/*.json')
+    error_json_path = glob.glob(pathname=f'{__checkstylerr_checkedout_projects_dir}/h2database-h2database/*/*/*.json')
     errors_info = unique([
         {
             'repo': path_splitted[-4],
@@ -45,27 +45,29 @@ def structure_real_error_dataset(errors_info):
         if n_errors not in dataset[error['repo']]:
             dataset[error['repo']][n_errors] = []
         dataset[error['repo']][n_errors] += [error]
-
     return dataset
 
 def create_dataset():
     if os.path.exists(__dataset_dir):
         shutil.rmtree(__dataset_dir)
-    errors_info = load_errors_info(only_targeted=True)
+    errors_info = load_errors_info(only_targeted=False)
     dataset = structure_real_error_dataset(errors_info)
     for project, number_of_errors_per_file in dataset.items():
         for number_of_errors, file_list in number_of_errors_per_file.items():
             if number_of_errors == 1:
-                for id, file_info in enumerate(file_list):
-                    dir = os.path.join(__dataset_dir, f'{project}/{number_of_errors}/{id}')
-                    metadata = {
-                        'commit': file_info['commit'],
-                        'file_name': file_info['filepath'].split('/')[-1],
-                        'errors': file_info['errors']
-                    }
-                    create_dir(dir)
-                    save_json(dir, 'metadata.json', metadata)
-                    shutil.copy(file_info['filepath'], os.path.join(dir,metadata['file_name']))
+                id = 0
+                for file_info in file_list:
+                    if file_info['errors'][0]['source'].split('.')[-1][:-5] in targeted_errors:
+                        dir = os.path.join(__dataset_dir, f'{project}/{number_of_errors}/{id}')
+                        metadata = {
+                            'commit': file_info['commit'],
+                            'file_name': file_info['filepath'].split('/')[-1],
+                            'errors': file_info['errors']
+                        }
+                        create_dir(dir)
+                        save_json(dir, 'metadata.json', metadata)
+                        shutil.copy(file_info['filepath'], os.path.join(dir,metadata['file_name']))
+                        id += 1
         shutil.copy(os.path.join(__checkstylerr_checkedout_projects_dir, f'{project}', 'checkstyle.xml'), os.path.join(__dataset_dir, f'{project}', 'checkstyle.xml'))
         shutil.copy(os.path.join(__checkstylerr_checkedout_projects_dir, f'{project}', 'info.json'), os.path.join(__dataset_dir, f'{project}', 'info.json'))
 
@@ -98,28 +100,41 @@ def checkstylerr_stats():
                         print(f'{error:<30} : {count}')
 
 def checkstylerr_stats_customized():
-    loaded_errors_info = load_errors_info(only_targeted=True)
-
-    number_of_projects = 0
+    loaded_errors_info = load_errors_info(only_targeted=False)
     
-    for repo, repo_errors_info in group_by(lambda e: e['repo'], loaded_errors_info).items():
+    repos = []
+    for repo_name, repo_errors_info in group_by(lambda e: e['repo'], loaded_errors_info).items():
         for size, errors_info in sorted(group_by(lambda e: len(e['errors']), repo_errors_info).items()):
-            if (size == 1):
+            if size == 1:
                 repo_errors = [
                     error['source'].split('.')[-1][:-5]
                     for info in errors_info
-                    if len(info['errors']) <= 10
                     for error in info['errors']
                 ]
-                if len(repo_errors) >= 20:
-                    number_of_projects += 1
-                    print(colored(f'{repo}: {len(repo_errors)/size:.0f} errors', attrs=['bold']))
-                    for error, count in dict_count(repo_errors).items():
-                        if error in targeted_errors:
-                            print('\t\t\t', end='')
-                            print(colored(f'{error:<30} : {count}', color='green'))
+                targeted_count = 0
+                only_targeted_errors = {}
+                for error, count in dict_count(repo_errors).items():
+                    if error in targeted_errors:
+                        targeted_count += count
+                        only_targeted_errors[error] = count
+                if targeted_count >= 20:
+                    repos.append({
+                        "repo_name": repo_name,
+                        "nb_errors": targeted_count,
+                        "nb_error_types": len(only_targeted_errors.items()),
+                        "errors": only_targeted_errors
+                    })
+                break
+    
+    for repo in sorted(repos, key=lambda item: (item['nb_error_types'], item['nb_errors']), reverse=True):
+        repo_name = repo['repo_name']
+        repo_nb_errors = repo['nb_errors']
+        print(colored(f'{repo_name}: {repo_nb_errors:.0f} errors', attrs=['bold']))
+        for error, count in sorted(repo['errors'].items(), key=lambda item: item[1], reverse=True):
+            print('\t\t', end='')
+            print(colored(f'{error:<30}\t{count}', color='green'))
 
-    print(f'Number of projects: {number_of_projects}')
+    print(f'Number of projects: {len(repos)}')
 
 if __name__ == '__main__':
     if sys.argv[1] == 'show-checkstylerr-stats':
